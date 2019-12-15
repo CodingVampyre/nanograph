2947380fb86ff6cb542de3df98affa2140dc004d
CodingVampyre <tovuko@hotmail.de>
2019-12-15 12:48:30 +0100

    writing tests

--- src/nanograph.spec.ts
+++ src/nanograph.spec.ts
@@ -1,11 +1,44 @@
-import {Minigraph} from './';
+import { Nanograph } from './';
 
 describe('create entities', () => {
 
 	describe('create vertices', () => {
 
 		test('create a vertex', () => {
-			const minigraph: Minigraph = new Minigraph();
+			const nano: Nanograph = new Nanograph();
+			const { id } = nano.create<string>('PERSON', 'John Doe');
+
+			expect(id).toBeDefined();
+			expect(nano.meta.getVertexCount()).toBe(1);
+		});
+
+		test('create a vertex using interfaces', () => {
+			interface IPerson { firstName: string, familyName: string, balance: number };
+			const nano: Nanograph = new Nanograph();
+			const { id } = nano.create<IPerson>('PERSON', {
+				firstName: 'John',
+				familyName: 'Doe',
+				balance: 42,
+			});
+
+			expect(id).toBeDefined();
+			expect(nano.meta.getVertexCount()).toBe(1);
+		});
+
+	});
+
+	describe('create edges', () => {
+
+		test('create edge', () => {
+
+		});
+
+		test('create Edge with properties', () => {
+
+		});
+
+		test('fail on non existing vertices', () => {
+
 		});
 
 	});
